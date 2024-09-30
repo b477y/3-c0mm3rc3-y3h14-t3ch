c350f19ea61c9636953b232fa758e899860ef4c0
b477y <mohammedsaleh002@outlook.com>
2024-09-30 15:35:16 +0300

    Add /cart/items/ POST endpoint

--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -1,14 +1,23 @@
 import Router from 'express'
 import { getActiveCartForUser } from '../services/cartService'
 import validateJWT from '../middlewares/validateJWT'
-import { ExtendRequest } from '../middlewares/validateJWT'
+import { ExtendRequest } from "../types/extendedRequest"
+import { addItemToCart } from "../services/cartService"
 
 const router = Router()
 
 router.get('/', validateJWT, async (req: ExtendRequest, res) => {
-    const userId = req.user._id
+    const userId = req?.user?._id
     const cart = await getActiveCartForUser({ userId })
     res.status(200).send(cart)
+})
+
+router.post('/items', validateJWT, async (req: ExtendRequest, res) => {
+    const userId = req?.user?._id
+    const { productId, quantity } = req.body;
+    const response = await addItemToCart({ userId, productId, quantity })
+    const { data, statusCode } = response
+    res.status(statusCode).send(data)
 })
 
 export default router;
